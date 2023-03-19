22512728bb28edb3aca57718a565db6aabcafa7b
alfiyaali2601 <alfiyaali177@gmail.com>
2023-03-19 14:02:59 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var headerTitle = document.getElementById('header-title');
+var item = document.getElementById("items");
+
+headerTitle.style.borderBottom = 'solid 3px #000'
+item.style.fontWeight = "bold"
+item.style.color = "green"
